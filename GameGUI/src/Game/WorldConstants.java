6cdfd1d7e0106acf146f5abd60ec7a6f1f9a09ed
Tomer Sitton <tomer.sitton1@gmail.com>
2018-06-03 01:36:21 +0300

    added distance from players

--- GameGUI/src/Game/WorldConstants.java
+++ GameGUI/src/Game/WorldConstants.java
@@ -17,9 +17,8 @@ public abstract class WorldConstants {
 		public static final int HEIGHT = 100;
 	}
 
-	public static interface Player1 {
-		public static final int X = 500;
-		public static final int Y = 50;
+	public static interface Players {
+		public static final int STARTING_DISTANCE = 200;
 	}
 
 	public static interface PHYSICS {
